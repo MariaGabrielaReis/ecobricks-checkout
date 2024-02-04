30d2d67f64fca1f719c6bcbea76e4297cbed2375
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2024-02-04 09:47:09 -0300

    feat: implements jwt (auth module)

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,21 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 
 import { AuthController } from './auth.controller';
+import { AuthGuard } from './auth.guard';
 import { AuthService } from './auth.service';
 
+@Global()
 @Module({
+  imports: [
+    JwtModule.register({
+      global: true,
+      secret: 'secret',
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, AuthGuard],
+  exports: [AuthGuard],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+
+const users = [
+  { id: 1, username: 'maby', password: 'maby' },
+  { id: 1, username: 'wllc', password: 'wllc' },
+];
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private jwtService: JwtService) {}
+
+  login(username: string, password: string) {
+    const user = users.find(
+      (user) => user.username === username && user.password === password,
+    );
+
+    if (!user) throw new UnauthorizedException();
+
+    const payload = { sub: user.id, username: user.username };
+    return { access_token: this.jwtService.sign(payload) };
+  }
+}
