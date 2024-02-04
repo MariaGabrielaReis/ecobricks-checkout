415d2dfc65a86d87dbef0ba2186b82f4f4b45b00
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2024-02-04 08:58:43 -0300

    feat: create order

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,16 +1,49 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { In, Repository } from 'typeorm';
 
+import { Product } from 'src/products/entities/product.entity';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { Order } from './entities/order.entity';
 
 @Injectable()
 export class OrdersService {
-  constructor(@InjectRepository(Order) private orderRepo: Repository<Order>) {}
+  constructor(
+    @InjectRepository(Order) private orderRepo: Repository<Order>,
+    @InjectRepository(Product) private productRepo: Repository<Product>,
+  ) {}
 
-  create(createOrderDto: CreateOrderDto) {
-    return 'This action adds a new order';
+  async create(createOrderDto: CreateOrderDto) {
+    const productIds = createOrderDto.items.map((item) => item.product_id);
+    const uniqueProductIds = [...new Set(productIds)];
+    const products = await this.productRepo.findBy({
+      id: In(uniqueProductIds),
+    });
+
+    if (products.length !== uniqueProductIds.length) {
+      throw new Error(
+        `Some product doesn't exist. Products: ${productIds}, find products: ${products.map(
+          (product) => product.id,
+        )}`,
+      );
+    }
+
+    const order = Order.create({
+      client_id: 1,
+      items: createOrderDto.items.map((item) => {
+        const product = products.find(
+          (product) => product.id === item.product_id,
+        );
+        return {
+          price: product.price,
+          product_id: item.product_id,
+          quantity: item.quantity,
+        };
+      }),
+    });
+    await this.orderRepo.save(order);
+
+    return order;
   }
 
   findAll() {
